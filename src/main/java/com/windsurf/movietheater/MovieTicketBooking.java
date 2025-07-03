7b9b497ae6e9eb0aa8b9bd29f5c7dc345c66fbea
m0h0m1f <mittapalli.hareesh@walmart.com>
2025-07-03 15:45:08 +0530

    Initial commit

---
+++ src/main/java/com/windsurf/movietheater/MovieTicketBooking.java
@@ -0,0 +1,13 @@
+package com.windsurf.movietheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBooking {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketBooking.class, args);
+    }
+
+}
---
+++ src/test/java/com/windsurf/movietheater/MovieTicketBookingTest.java
@@ -0,0 +1,13 @@
+package com.windsurf.movietheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
